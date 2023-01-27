d53f88be3f3e1b7deedddced6b4e655ba6a760fe
Bogdoir <77168551+Bogdoir@users.noreply.github.com>
2023-01-27 23:08:58 +0000

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php $title = "Clarity's Home"; include_once "C:/xampp/htdocs/BlogSite/assets/html/header.html"; ?>
-	<main id="Home">
-		<div class="Container">
-			<h1>Welcome!</h1>
-			<hr>
-			<p>~ ~ ~</p>
-		</div>
-	</main>
-<?php include_once "C:/xampp/htdocs/BlogSite/assets/html/footer.html"; ?>
